a4961f5145c7a632bd6033ed22e56eb1bd12d52b
Robert Allen <rfelixallen@gmail.com>
2014-10-06 21:32:40 -0400

    copied a demo program that prints characters. Could be good for intro text to something.

---
+++ listing.c
@@ -0,0 +1,23 @@
+#include <ncurses.h>
+
+int main(void)
+{
+	char text[] = "Greetings from NCurses!";
+	char *t;
+
+	initscr();
+	t = text;
+
+	while (*t)
+	{
+		addch(*t);
+		t++;
+		refresh();
+		napms(100);
+	}
+	getch();
+
+	endwin();
+	return 0;
+
+}
