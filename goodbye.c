8331630749bcdb49cba67c8964ddda4ded49f76f
Robert Allen <rfelixallen@gmail.com>
2014-10-06 21:02:55 -0400

    Commented the program

--- goodbye.c
+++ goodbye.c
@@ -4,11 +4,15 @@
 
 int main(void)
 {
+	// intscr starts NCurses and creates the strscr and curscr.
 	initscr();
+	// addstr adds a string to the standard screen.
 	addstr("Goodbye, cruel C programming!");
+	// refresh updates the virtual screen witht he new input, and pushes the input from the virtual scr to the cur scr.
 	refresh();
+	// getch pauses the program and waits for user input.
 	getch();
-
+	// endwin shuts down NCurses.
 	endwin();
 	return 0;
 }
