5754bad926159008ea252e45a0d8db5c96916a66
Robert Allen <rfelixallen@gmail.com>
2014-10-06 21:42:12 -0400

    made a new add file to test adding strings

---
+++ add2.c
@@ -0,0 +1,16 @@
+#include <ncurses.h>
+
+int main(void)
+{
+	char text1[] = "Oh give me a clone!\n";
+	char text2[] = "Yes a clone of my own!";
+
+	initscr();
+	addstr(text1);
+	addstr(text2);
+	refresh();
+	getch();
+
+	endwin();
+	return 0;
+}
