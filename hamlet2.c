0e9dc187a86d4f6b063012991039e32c289916be
Robert Allen <rfelixallen@gmail.com>
2014-10-18 16:41:45 -0400

    demonstrates adding a line

---
+++ hamlet2.c
@@ -0,0 +1,29 @@
+#include <ncurses.h>
+
+// Prints 3 lines. Staging for later demonstrations.
+
+int main(void)
+{
+	char Ham1[] = "To be, or not to be: that is the quesiton:\n";
+	char Ham2[] = "Whether tis nobler in the mind to suffer\n";
+	char Ham3[] = "The slings and arrows of outrageous fortune,\n";
+	char Ham4[] = "Or to take arms against a sea of troubles,\n";
+	char Ham5[] = "And by opposing end them?\n";
+
+	initscr();
+
+	addstr(Ham1);
+	addstr(Ham3);
+	addstr(Ham5);
+	refresh();
+	getch();		// Wait for key press
+
+	move(1,0);
+	insertln();		// Inserts a blank line, scroll text down
+	addstr(Ham2);	// line to insert
+	refresh();
+	getch();
+	
+	endwin();
+	return 0;
+}
