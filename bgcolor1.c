6603e69932bc41f537ecd970a4a90d8aa57b5199
Robert Allen <rfelixallen@gmail.com>
2014-10-18 13:01:09 -0400

    New samples files

---
+++ bgcolor1.c
@@ -0,0 +1,15 @@
+#include <ncurses.h>
+
+int main(void);
+{
+	initscr();
+
+	start_color();
+	init_pair(1,COLOR_WHITE,COLOR_BLUE);
+	bkgd(COLOR_PAIR(1));
+	refresh();
+	getch();
+
+	endwin();
+	return 0;
+}
